0bba94be5e87ee221496cb4563bcc391501f065c
skyfire <skyfireitdiy@hotmail.com>
2019-08-09 09:45:34 +0800

    配置docker环境

---
+++ blog_build_docker/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+mkdir -p /opt/my_blog/db
+cd /opt/my_blog/
+rm -rf build blog_server
+mkdir build
+cd build
+cmake ..
+make
+cd src
+cp blog_server /opt/my_blog/
+cd /opt/my_blog
+./blog_server
---
+++ make_docker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker build -t cpp_build_docker cpp_build_docker
+docker build -t blog_build_docker  blog_build_docker
