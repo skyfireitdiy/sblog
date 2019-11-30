314168082bf4c39e29d3189705be0eab11a7692d
skyfire <skyfireitdiy@hotmail.com>
2019-11-30 21:08:23 +0800

    增加dot支持

--- static/js/tools.js
+++ static/js/tools.js
@@ -34,12 +34,17 @@ let markdown_converter = function() {
                 type: 'output',
                 filter: function(text, converter, options) {
                     // use new shodown's regexp engine to conditionally parse codeblocks
-                    var left = '<pre><code\\b[^>]*>',
+                    let left = '<pre><code\\b[^>]*>',
                         right = '</code></pre>',
                         flags = 'g',
                         replacement = function(wholeMatch, match, left, right) {
                             // unescape match to prevent double escaping
                             match = htmlunencode(match);
+                            if (left == "<pre><code class=\"dot language-dot\">") {
+                                const workerURL = '/js/full.render.js';
+                                match = Viz(match, "svg");
+                                return match;
+                            }
                             return left + hljs.highlightAuto(match).value + right;
                         };
                     return showdown.helper.replaceRecursiveRegExp(text, replacement, left, right, flags);
